4644939844df409f44fe6d5b25005418957ccb0f
surya-situ <suryasocial55@gmail.com>
2024-11-06 17:56:31 +0530

    feat: Set up Redux store with video slice for state management.

---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import videoReducer from "./videoSlice";
+
+const appStore = configureStore (
+    {
+        reducer: {
+            videos: videoReducer
+        }
+    }
+);
+
+export default appStore;
